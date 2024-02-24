a43a382f505504d1dd539528af46b45fef7078c3
Cavaari <cavaarit@gmail.com>
2024-02-24 01:12:49 -0500

    profile photo routing fix

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -13,7 +13,7 @@ function AboutMe() {
         <div className="about-content">
           <h1>About Cavaari</h1>
         <div className="profile-picture">
-          <img src="../images/Profile Photo.jpg" alt="Profile" />
+          <img src="./images/Profile Photo.jpg" alt="Profile" />
         </div>
           <p>
             "I am a dedicated fourth-year university student at the University of Guelph, majoring in Software Engineering
