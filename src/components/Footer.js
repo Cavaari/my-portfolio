730b710cbba1ba6a77d70d3429bf13b1fa8a9cd7
Cavaari <cavaarit@gmail.com>
2024-02-24 01:50:04 -0500

    Footer fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,57 +18,56 @@ function Footer() {
         <div className='footer-link-wrapper'>
             <div className='footer-link-items'>
                 <h2>About Me</h2>
-                <a href='/Experience'>Experience</a>
-                <a href='/Projects'>Projects</a>
-                <a href='/Skills'>Skills</a>
+                <Link to='/Experience'>Experience</Link>
+                <Link to='/Projects'>Projects</Link>
+                <Link to='/Skills'>Skills</Link>
             </div>
-                        <div className='footer-link-items'>
-                            <h2>Contact Me <FaPhoneAlt /></h2>
-                            <a href='tel:+1(226)-979-2533'>Cell</a>
-                            <a href='mailto:cavaarit@gmail.com'>Email <FaEnvelope /></a>
-                            <a href='https://linkedin.com/in/cavaari-taylor-8976a6234'>LinkedIn</a>
-                        </div>
+            <div className='footer-link-items'>
+                <h2>Contact Me <FaPhoneAlt /></h2>
+                <a href='tel:+1(226)-979-2533'>Cell</a>
+                <a href='mailto:cavaarit@gmail.com'>Email <FaEnvelope /></a>
+                <a href='https://linkedin.com/in/cavaari-taylor-8976a6234'>LinkedIn</a>
             </div>
-            <section className='social-media'>
-                <div className='social-media-wrap'>
-                    <div className='footer-logo'>
-                        <a href='/my-portfolio' className='social-logo'>
-                        ©
-                        </a>
-                    </div>
-                    <small className='website-rights'>© 2024</small>
-                    <div className='social-icons'>
-                        <Link
-                        class='social-icon-link github'
-                        to='https://github.com/Cavaari'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        aria-label='github'
-                        >
-                        <i class='fab fa-github' />
-                        </Link>
-                        <Link
-                        class='social-icon-link LinkedIn'
-                        to='https://linkedin.com/in/cavaari-taylor-8976a6234'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        aria-label='LinkedIn'
-                        >
-                        <i class='fab fa-linkedin' />
-                        </Link>
-                        <Link
-                        className='social-icon-link gmail'
-                        to='mailto:cavaarit@gmail.com'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        aria-label='Gmail'
-                        >
-                        <FaEnvelope />
-                        </Link>
-                    </div>
+        </div>
+        <section className='social-media'>
+            <div className='social-media-wrap'>
+                <div className='footer-logo'>
+                    <a href='/my-portfolio' className='social-logo'>
+                    ©
+                    </a>
                 </div>
-            </section>
-        
+                <small className='website-rights'>© 2024</small>
+                <div className='social-icons'>
+                    <Link
+                    class='social-icon-link github'
+                    to='https://github.com/Cavaari'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='github'
+                    >
+                    <i class='fab fa-github' />
+                    </Link>
+                    <Link
+                    class='social-icon-link LinkedIn'
+                    to='https://linkedin.com/in/cavaari-taylor-8976a6234'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='LinkedIn'
+                    >
+                    <i class='fab fa-linkedin' />
+                    </Link>
+                    <Link
+                    className='social-icon-link gmail'
+                    to='mailto:cavaarit@gmail.com'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='Gmail'
+                    >
+                    <FaEnvelope />
+                    </Link>
+                </div>
+            </div>
+        </section>
         </div>
   )
 }
